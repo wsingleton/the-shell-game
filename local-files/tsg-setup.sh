3e83eab2f5706388ffdc09245e36682dc4e09558
Wezley <wezley.singleton@gmail.com>
2021-07-25 22:03:02 -0400

    update setup script to correctly unpack files; update create user to lock .bashrc

--- local-files/tsg-setup.sh
+++ local-files/tsg-setup.sh
@@ -4,7 +4,7 @@ pemFile=$1
 connectionStr=$2
 
 getTsgTar="aws s3 cp s3://the-shell-game/tsg.tar.gz ~/tsg.tar.gz"
-unpackTsgTar="sudo tar -xzf ~/tsg.tar.gz && mv ~/tsg/* . && rm -Rf ~/tsg/"
+unpackTsgTar="sudo tar -xzf ~/tsg.tar.gz && mv ~/remote-files/* . && rm -Rf ~/remote-files/"
 setNprocLimit="sudo sh setup-user-nproc-limit.sh"
 
 echo "$getTsgTar; $unpackTsgTar; $setNprocLimit; exit" | ssh -tt -i $pemFile $connectionStr
